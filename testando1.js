a248e0c6c0eef9e87366fe914a0d6c0066a09764
Guilherme Silva <dasilva.guilherme.582@gmail.com>
2022-03-25 15:51:45 -0300

    update #5

--- testando1.js
+++ testando1.js
@@ -7,17 +7,3 @@ function somar(){
     var s = n1 + n2
     res.innerText = `A soma entre ${n1} e ${n2} é igual a ${s}.`
 }
-
-function minhaFuncao() {
-    var cid = window.document.querySelector('#cidade').value
-    var ress = window.document.querySelector('div#ress')
-    
-    var cid1 = String(texto.value)
-    
-    if( cid != "Capital" ){
-    ress.innerHTML = `<p>Você nasceu em ${cid1} uma cidade na ${cid}, logo você é <strong>Paulista</strong></p>`
-    } else {
-    ress.innerHTML = `<p>Você nasceu em ${cid1} uma cidade na ${cid}, logo você é <strong>Paulistano</strong></p>`
-    } 
-    
-}
